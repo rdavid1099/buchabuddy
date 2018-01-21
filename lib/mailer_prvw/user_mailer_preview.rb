2437aa58d8197aa6b01d0e1963c1b30082b7a67c
Ryan Workman <rdavid1099@gmail.com>
2018-01-20 17:38:53 -0700

    Fix font-weight in mailer to match page styling

--- lib/mailer_prvw/user_mailer_preview.rb
+++ lib/mailer_prvw/user_mailer_preview.rb
@@ -3,6 +3,14 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.confirmation_instructions(User.first, {})
   end
 
+  def email_changed
+    UserMailer.email_changed(User.first, {})
+  end
+
+  def password_change
+    UserMailer.password_change(User.first, {})
+  end
+
   def reset_password_instructions
     UserMailer.reset_password_instructions(User.first, {})
   end
