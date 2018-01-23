456844c8029e4671fbb2139a35f988312076900d
Ryan Workman <rdavid1099@gmail.com>
2018-01-22 17:18:21 -0700

    Add tooltip for account creation password reqs

--- app/assets/javascripts/users/modals/signInModal.js
+++ app/assets/javascripts/users/modals/signInModal.js
@@ -31,6 +31,36 @@ const fields = {
           '</form>',
     Footer: '<small class="form-text text-muted">Don\'t have an account? <span class="style-link" onclick="renderField(\'newAccount\')">Sign up</span></small>' +
             '<small class="form-text text-muted">Already have an account? <span class="style-link" onclick="renderField(\'signIn\')">Sign in</span></small>'
+  },
+
+  'newAccount': {
+    LongTitle: 'Create Account',
+    Body: '<form class="new_user" id="new_user" action="/users" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="BQYmqJh7THJLy+sx5f5WH8KwQSVZfReto4FzsBnHYGxyDfzl/pKYMUyIeh6z35imVCFiSKG+SduSDSAUqHvmcA==">' +
+            '<div class="field form-group">' +
+              '<input class="form-control" placeholder="Email Address" autofocus="autofocus" type="email" value="" name="user[email]" id="user_email">' +
+            '</div>' +
+            '<div class="field form-group">' +
+              '<input class="form-control" placeholder="Password" autocomplete="off" type="password" name="user[password]" id="user_password" onfocus="displayTooltip()" onblur="removeTooltip()">' +
+              '<div class="tooltip bs-tooltip-right" role="tooltip" id="password-reqs">' +
+                '<div class="arrow"></div>' +
+                '<div class="tooltip-inner">' +
+                  '<ul>' +
+                    '<li><em>At least 6 characters</em></li>' +
+                    '<li><em>One uppercase letter</em></li>' +
+                    '<li><em>One lowercase letter</em></li>' +
+                    '<li><em>One number letter</em></li>' +
+                  '</ul>' +
+                '</div>' +
+              '</div>' +
+            '</div>' +
+            '<div class="field form-group">' +
+              '<input class="form-control" placeholder="Password Confirmation" autocomplete="off" type="password" name="user[password_confirmation]" id="user_password_confirmation">' +
+            '</div>' +
+            '<div class="actions">' +
+              '<input class="btn btn-primary submit" type="submit" name="commit" value="Register" data-disable-with="Register">' +
+            '</div>' +
+          '</form>',
+    Footer: '<small class="form-text text-muted">Already have an account? <span class="style-link" onclick="renderField(\'signIn\')">Sign in</span></small>'
   }
 }
 
@@ -38,4 +68,12 @@ const renderField = (type) => {
   Object.keys(fields[type]).forEach((key) => {
     document.getElementById(`signInModal${key}`).innerHTML = fields[type][key]
   })
+}
+
+const displayTooltip = () => {
+  document.getElementById('password-reqs').classList.add('show')
+}
+
+const removeTooltip = () => {
+  document.getElementById('password-reqs').classList.remove('show')
 }
