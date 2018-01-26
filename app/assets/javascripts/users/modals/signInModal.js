9f75b298f5a5b2dad1d73b7216288b8ca7963e76
Ryan Workman <rdavid1099@gmail.com>
2018-01-25 17:30:01 -0700

    Add validation to forgot password modal

--- app/assets/javascripts/users/modals/signInModal.js
+++ app/assets/javascripts/users/modals/signInModal.js
@@ -22,11 +22,12 @@ const fields = {
     Body: '<form class="new_user" id="new_user" action="/users/password" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="G6L8/0QITGObq2cXmfG2G2jhClkJQj+f/av22xEV2JuTF4QWkwYTTs3ssCBv6pitJswXdUl0NQWZKQdJ89RkOg==">' +
             '<div class="field form-group">' +
               '<label for="user_email">Enter your email address, and we\'ll send you a recovery link.</label>' +
-              '<input class="form-control" placeholder="Email Address" autofocus="autofocus" type="email" value="" name="user[email]" id="user_email">' +
+              '<input class="form-control" placeholder="Email Address" autofocus="autofocus" type="email" value="" name="user[email]" id="user_email" onkeyup="checkEmail(event, evaluateSubmitValidity, true, 2)" onblur="checkEmail(event, evaluateSubmitValidity, true, 2)">' +
+              '<div class="errors"></div>' +
               '<small class="form-text style-link" onclick="renderField(\'confirmable\')">Resend Confirmation Email</small>' +
             '</div>' +
-            '<div class="actions">' +
-              '<input class="btn btn-primary submit" type="submit" name="commit" value="Send Recovery Email" data-disable-with="Send Recovery Email">' +
+            '<div class="actions" style="pointer-events: none;">' +
+              '<input class="btn btn-primary submit disabled" id="submit" type="submit" name="commit" value="Send Recovery Email" data-disable-with="Send Recovery Email">' +
             '</div>' +
           '</form>',
     Footer: '<small class="form-text text-muted">Don\'t have an account? <span class="style-link" onclick="renderField(\'newAccount\')">Sign up</span></small>' +
@@ -60,7 +61,7 @@ const fields = {
               '<div class="errors"></div>' +
             '</div>' +
             '<div class="actions" style="pointer-events: none;">' +
-              '<input class="btn btn-primary submit disabled" id="new-user-submit" type="submit" name="commit" value="Register" data-disable-with="Register">' +
+              '<input class="btn btn-primary submit disabled" id="submit" type="submit" name="commit" value="Register" data-disable-with="Register">' +
             '</div>' +
           '</form>',
     Footer: '<small class="form-text text-muted">Already have an account? <span class="style-link" onclick="renderField(\'signIn\')">Sign in</span></small>'
@@ -87,18 +88,19 @@ const removeTooltip = () => {
   document.getElementById('password-reqs').classList.remove('show')
 }
 
-const checkEmail = (e, cb) => {
-  let $emailInput = e.currentTarget
+const checkEmail = (e, cb, onlyEmail, index) => {
+  let $emailInput = e.currentTarget,
+      i           = index || 1
   if ($emailInput.value) {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($emailInput.value)) {
-      validate('email', $emailInput)
+      validate('email', $emailInput, i)
     } else {
-      invalidate('email', $emailInput, 'Invalid email address')
+      invalidate('email', $emailInput, 'Invalid email address', i)
     }
   } else {
-    invalidate('email', $emailInput, 'Required')
+    invalidate('email', $emailInput, 'Required', i)
   }
-  if(cb) {cb()}
+  if(cb) {cb(onlyEmail)}
 }
 
 const checkPassword = (e, cb) => {
@@ -107,9 +109,9 @@ const checkPassword = (e, cb) => {
   if ($pwInput.value) {
     let validations = [($pwInput.value.length >= 6), (/[A-Z]/.test($pwInput.value)), (/[a-z]/.test($pwInput.value)), (/[0-9]/.test($pwInput.value))]
     if (!validations.includes(false)) {
-      validate('password', $pwInput)
+      validate('password', $pwInput, 2)
     } else {
-      invalidate('password', $pwInput, 'Password must meet all requirements')
+      invalidate('password', $pwInput, 'Password must meet all requirements', 2)
     }
     for (var i = 0; i < $reqList.children.length; i++) {
       if (validations[i]) {
@@ -125,7 +127,7 @@ const checkPassword = (e, cb) => {
       $reqList.children[i].classList.add('is-invalid')
       $reqList.children[i].classList.remove('is-valid')
     }
-    invalidate('password', $pwInput, 'Required')
+    invalidate('password', $pwInput, 'Required', 2)
   }
   if(cb) {cb()}
 }
@@ -134,16 +136,16 @@ const checkPasswordConfirm = (e, cb) => {
   let $pwConfirmInput = e.currentTarget,
       $pwInput        = e.target.parentElement.previousElementSibling.firstElementChild
   if ($pwConfirmInput.value) {
-    $pwConfirmInput.value === $pwInput.value ? validate('passwordConfirm', $pwConfirmInput) : invalidate('passwordConfirm', $pwConfirmInput, 'Passwords do not match')
+    $pwConfirmInput.value === $pwInput.value ? validate('passwordConfirm', $pwConfirmInput, 1) : invalidate('passwordConfirm', $pwConfirmInput, 'Passwords do not match', 1)
   } else {
-    invalidate('passwordConfirm', $pwConfirmInput, 'Required')
+    invalidate('passwordConfirm', $pwConfirmInput, 'Required', 1)
   }
   if(cb) {cb()}
 }
 
-const evaluateSubmitValidity = () => {
-  let $registerBtn = document.getElementById('new-user-submit')
-  if (signUpValidations.email && signUpValidations.password && signUpValidations.passwordConfirm) {
+const evaluateSubmitValidity = (onlyEmail) => {
+  let $registerBtn = document.getElementById('submit')
+  if ((signUpValidations.email && signUpValidations.password && signUpValidations.passwordConfirm) || (onlyEmail && signUpValidations.email)) {
     $registerBtn.classList.remove('disabled')
     $registerBtn.parentElement.style.pointerEvents = "all"
   } else {
@@ -152,8 +154,7 @@ const evaluateSubmitValidity = () => {
   }
 }
 
-const validate = (field, input) => {
-  let index = (field === 'password') ? 2 : 1
+const validate = (field, input, index) => {
   input.classList.remove('is-invalid')
   input.classList.add('is-valid')
   input.parentElement.children[index].classList.remove('invalid-feedback')
@@ -161,8 +162,7 @@ const validate = (field, input) => {
   signUpValidations[field] = true
 }
 
-const invalidate = (field, input, msg) => {
-  let index = (field === 'password') ? 2 : 1
+const invalidate = (field, input, msg, index) => {
   input.classList.remove('is-valid')
   input.classList.add('is-invalid')
   input.parentElement.children[index].classList.add('invalid-feedback')
