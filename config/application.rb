ff4cef39b19e17a23dc958c095f8694dbd4890fd
Ryan Workman <rdavid1099@gmail.com>
2018-01-28 16:49:48 -0700

    Add Angular view to render in Rails

--- config/application.rb
+++ config/application.rb
@@ -18,15 +18,16 @@ Bundler.require(*Rails.groups)
 
 module Buchabuddy10
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # config.middleware.insert_before 0, 'Rack::Cors' do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #   end
+    # end
 
     config.active_job.queue_adapter = :sidekiq
   end
