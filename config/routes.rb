52fa4f0f58a0a2e1c1ad52642e94863c91ee15de
Ryan Workman <rdavid1099@gmail.com>
2018-02-14 11:41:27 -0700

    Monkeypatch account creation to accept and save username parameter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'overrides/registrations'
+  }
 
   root to: 'bucha_buddy#index'
 end
