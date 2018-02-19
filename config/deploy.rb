f74522fffcfe11c5d495cafa226067eec199d780
Ryan Workman <rdavid1099@gmail.com>
2018-02-19 15:11:36 -0700

    Pull github using https

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.1"
 
 set :application, "buchabuddy"
-set :repo_url, "git@github.com:buchabuddy/buchabuddy.git"
+set :repo_url, "https://github.com/buchabuddy/buchabuddy.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
