2985cc4197c1c401ec35073d35bec3507c033e84
Ryan Workman <rdavid1099@gmail.com>
2018-03-06 11:59:09 -0700

    Add check for write permission to deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.10.1"
 set :application, "buchabuddy"
 set :repo_url, "git@github.com:buchabuddy/buchabuddy.git"
 
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 set :deploy_to, "/home/deploy/buchabuddy"
 
@@ -12,14 +12,16 @@ append :linked_files, "config/database.yml", "config/secrets.yml", "./.secrets"
 
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", "public/uploads"
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+namespace :deploy do
+  task :check_write_permissions do
+    on roles(:all) do |host|
+      if test("[ -w #{fetch(:deploy_to)} ]")
+        info "#{fetch(:deploy_to)} is writable on #{host}"
+      else
+        error "#{fetch(:deploy_to)} is not writable on #{host}"
+      end
+    end
+  end
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  before :starting, :check_write_permissions
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,1 @@
 server "159.89.149.249", user: "deploy", roles: %w{app db web}
-
-before :starting, :check_write_permissions
